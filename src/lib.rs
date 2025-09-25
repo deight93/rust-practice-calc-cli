b91685472be5346cd5d75e579c1d49106d82888a
deight93 <deight8311@gmail.com>
2025-09-25 17:23:05 +0900

    feat: add compute func

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,11 @@ pub mod error;
 pub mod eval;
 pub mod parser;
 pub mod types;
+
+use error::CalcError;
+
+pub fn compute(expr: &str) -> Result<f64, CalcError> {
+    let parsed = parser::parse(expr)?;
+    let value = eval::evaluate(parsed)?;
+    Ok(value)
+}
